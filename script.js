50947be81ceec8d254492821eaef0479e6bf1060
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-04 00:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded", () => {
+	const inp=document.querySelectorAll('.code');
+
+	// const codes = document.querySelectorAll('.code');
+	inp.forEach((txt,index)=>{
+		txt.addEventListener('input',(e)=>{
+			if(e.target.value.length===1&&index<inp.length-1){
+				inp[index+1].focus();
+			}
+		});
+	
+		txt.addEventListener('keydown', (e)=>{
+			if(e.key=="Backspace"&&index>0&&e.target.value==''){
+				inp[index - 1].focus();
+			}
+		});
+		
+	});
+});
+// // script.js
+// document.addEventListener("DOMContentLoaded", () => {
+//     const codes = document.querySelectorAll('.code');
+    
+//     codes.forEach((code, index) => {
+//         code.addEventListener('input', (e) => {
+//             if (e.target.value.length === 1 && index < codes.length - 1) {
+//                 codes[index + 1].focus();
+//             }
+//         });
+
+//         code.addEventListener('keydown', (e) => {
+//             if (e.key === 'Backspace' && index > 0 && e.target.value === '') {
+//                 codes[index - 1].focus();
+//             }
+//         });
+//     });
+// });
